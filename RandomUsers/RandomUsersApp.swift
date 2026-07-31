2aed5e1aa1e04e9384f48f4315d1823ce57d1aa9
morealcode <235274488+morealcode@users.noreply.github.com>
2026-07-31 14:31:31 +0200

    Initial commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by morealcode on 31/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
